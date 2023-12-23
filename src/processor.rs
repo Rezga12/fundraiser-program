39dc5c1700a976658317cd181f4230c347e9abc9
Rezga12 <rmeis16@freeuni.edu.ge>
2023-12-23 16:28:32 +0400

    fix something

--- src/processor.rs
+++ src/processor.rs
@@ -1,4 +1,4 @@
-use solana_program::{entrypoint::ProgramResult, account_info::{AccountInfo, next_account_info}, pubkey::Pubkey, system_program, program_error::ProgramError, system_instruction, program::{invoke, invoke_signed}, rent::Rent, sysvar::Sysvar};
+use solana_program::{entrypoint::ProgramResult, account_info::{AccountInfo, next_account_info}, pubkey::Pubkey, system_program, program_error::ProgramError, system_instruction, program::{invoke, invoke_signed}, rent::Rent, sysvar::Sysvar, msg};
 
 use crate::{instruction::{FundraiserInstruction, InitializeInstructionData, FundInstruction}, state::Fundraiser};
 
@@ -8,22 +8,34 @@ impl Processor {
     pub fn process_initialize(
         program_id: &Pubkey, 
         accounts: &[AccountInfo], 
-        instruction: InitializeInstructionData) -> ProgramResult {
+        instruction: InitializeInstructionData
+    ) -> ProgramResult {
 
         let iter = &mut accounts.iter();
 
+        // program derived address
         let initializer_account = next_account_info(iter)?;
         let balance_pda_account = next_account_info(iter)?;
         let fundraser_state_account = next_account_info(iter)?;
         let system_program = next_account_info(iter)?;
 
+
+        // rent exempt
         let rent = Rent::get()?;
         let minimum_balance = rent.minimum_balance(73);
 
         let (state_account_pda, bump) = Pubkey::find_program_address(
-            &[b"fundraiser", initializer_account.key.as_ref(), &[instruction.extra_seed]], 
+            &[
+                b"fundraiser", 
+                initializer_account.key.as_ref(), 
+                &[instruction.extra_seed]
+            ],
             program_id
         );
+
+        if state_account_pda != *fundraser_state_account.key {
+            return Err(ProgramError::Custom(1));
+        }
 
         let create_ix = system_instruction::create_account(
             initializer_account.key, 
@@ -40,25 +52,21 @@ impl Processor {
             &[
                 b"fundraiser", 
                 initializer_account.key.as_ref(),
+                &[instruction.extra_seed],
                 &[bump]
             ]
         ])?;
 
         let mut fundraiser_state = Fundraiser::unpack_from_account(fundraser_state_account)?;
-        if fundraiser_state.is_initialized {
-            return Err(ProgramError::AccountAlreadyInitialized);
-        }
-
-
-
-
-
-        if state_account_pda != *fundraser_state_account.key {
-            return Err(ProgramError::Custom(1));
-        }
+        // if fundraiser_state.is_initialized {
+        //     return Err(ProgramError::AccountAlreadyInitialized);
+        // }
 
         let (balance_calculated_pda, bump) = Pubkey::find_program_address(
-            &[b"balance", fundraser_state_account.key.as_ref()], 
+            &[
+                b"balance", 
+                fundraser_state_account.key.as_ref()
+            ], 
             program_id
         );
 
@@ -83,11 +91,17 @@ impl Processor {
 
         let iter = &mut accounts.iter();
 
+
+        
+
         let funder_account = next_account_info(iter)?;
         let funding_pda = next_account_info(iter)?;
         let system_program = next_account_info(iter)?;
 
-        let ix = system_instruction::transfer(funder_account.key, funding_pda.key, instruction.funding_amount);
+        let ix = system_instruction::transfer(
+                funder_account.key,
+                 funding_pda.key, 
+                 instruction.funding_amount);
         invoke(&ix, &[
             funder_account.clone(),
             funding_pda.clone(),
@@ -101,14 +115,54 @@ impl Processor {
 
         let iter = &mut accounts.iter();
         let initializer_account = next_account_info(iter)?;
-        let funder_pda = next_account_info(iter)?;
+        let balance_pda = next_account_info(iter)?;
         let fundraiser_state_account = next_account_info(iter)?;
         let system_program = next_account_info(iter)?;
 
+
+        let state = Fundraiser::unpack_from_account(fundraiser_state_account)?;
+
+        
+        
+        if state.goal_amount > balance_pda.lamports() {
+            return Err(ProgramError::Custom(1));
+        }
+
+        let ix = system_instruction::transfer(
+            balance_pda.key, 
+            initializer_account.key, 
+            balance_pda.lamports()
+        );
+
+        let (balance_calculated_pda, bump) = Pubkey::find_program_address(
+            &[
+                b"balance", 
+                fundraiser_state_account.key.as_ref()
+            ], 
+            program_id
+        );
+
+        invoke_signed(
+            &ix, 
+            &[
+                balance_pda.clone(),
+                initializer_account.clone()
+            ], 
+            &[
+                &[
+                    b"balance",
+                    fundraiser_state_account.key.as_ref(),
+                    &[bump]
+                ]
+            ]
+        )?;
+
+        // transfer to initializer account
         **initializer_account.lamports.borrow_mut() = initializer_account
             .lamports()
             .checked_add(fundraiser_state_account.lamports())
             .ok_or(ProgramError::InsufficientFunds)?;
+
         **fundraiser_state_account.lamports.borrow_mut() = 0;
         
 
